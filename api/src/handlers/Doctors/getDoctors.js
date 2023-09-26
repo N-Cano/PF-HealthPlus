5c36c581883f30347a8321f7e6f98123ecea944b
Manu <manuharris0@gmail.com>
2023-09-26 14:48:04 -0300

    feat(api/src/handlers): json hardcodeado

--- api/src/handlers/Doctors/getDoctors.js
+++ api/src/handlers/Doctors/getDoctors.js
@@ -1,7 +1,22 @@
+const doctors = [
+    {name: 'Manu', specialty: 'Urology', price: 200},
+    {name: 'Nacho', specialty: 'Dermatology', price: 120},
+    {name: 'Paula', specialty: 'Endocrinology', price: 80},
+    {name: 'Kate', specialty: 'Rheumatology', price: 40},
+    {name: 'Lauty', specialty: 'Radiology', price: 150},
+    {name: 'Jorge', specialty: 'Geriatrics', price: 320},
+    {name: 'Nahu', specialty: 'Oncology', price: 70},
+    {name: 'Leo', specialty: 'Psychiatry', price: 120},
+    {name: 'Messi', specialty: 'Cardiology', price: 80},
+    {name: 'Riquelme', specialty: 'General Doctor', price: 35},
+    {name: 'Mirtha Legran', specialty: 'Obstetrics and Gynecology', price: 120},
+    {name: 'Esto', specialty: 'Ophthalmology', price: 130},
+    {name: 'Es', specialty: 'Rhinology/Nasal and Sinus Care', price: 100},
+    {name: 'BOKAAAA', specialty: 'Chiropodist', price: 120},
+]
+
 const getDoctors = (req, res) => {
-    res.status(200).json({
-        status: 'emulo que traigo info de los doctores'
-    })
+    res.status(200).json(doctors)
 };
 
 module.exports = getDoctors;
