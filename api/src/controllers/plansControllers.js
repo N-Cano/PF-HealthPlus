3ee68ef39def586532c6e706b8c94ae2f4414326
Nacho Polinari <nachopolinari@gmail.com>
2023-09-27 15:27:41 -0300

    feat(api): handler & controller allPlans from BD

---
+++ api/src/controllers/plansControllers.js
@@ -0,0 +1,18 @@
+const { db } = require('../firebase');
+
+// --- Bring all plans from data base ---
+
+const bringPlans = async () => {
+    try {
+        const allPlans = await db.collection('plans').get()
+
+        const plans = allPlans.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        return plans;
+    } catch (error) {
+        throw new Error(error)
+    }
+};
+module.exports = {bringPlans};
