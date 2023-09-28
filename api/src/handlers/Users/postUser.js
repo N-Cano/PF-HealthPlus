b14a25df89d4c1ea4db3c7371a3edfeca9066ac3
Manu <manuharris0@gmail.com>
2023-09-28 09:57:23 -0300

    feat(api/src): Sign up validation

--- api/src/handlers/Users/postUser.js
+++ api/src/handlers/Users/postUser.js
@@ -1,11 +1,17 @@
-const createUser = require('../../controllers/usersControllers')
+const { createUser } = require('../../controllers/usersControllers')
 
 const postUser = async (req, res) => {
-    const { name, email, password, personalId, image, location, plan } = req.body;
+    try {
+        const { name, email, password, personalId, image, location, plan } = req.body;
 
-    const data = { name, email, password, personalId, image, location, plan }
-    const user = await createUser(data)
-    res.status(201).json(user)
+        if (!name || !email || !password) throw new Error('Missing information to create new user')
+
+        const data = { name, email, password, personalId, image, location, plan }
+        const user = await createUser(data)
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(400).json(error)
+    }
 };
 
 module.exports = postUser
