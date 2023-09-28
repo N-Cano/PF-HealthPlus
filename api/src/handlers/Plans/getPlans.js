ff0574b250bcb0b8a4c5db507791de8552800706
Manu <manuharris0@gmail.com>
2023-09-28 09:54:09 -0300

    delete(api): plans router

--- api/src/handlers/Plans/getPlans.js
+++
@@ -1,7 +0,0 @@
-const getPlans =(req,res)=>{
-res.status(200).json({
-    status: 'Simulo que trae todos los planes'
-})
-};
-
-module.exports= getPlans;
--- api/src/routes/plansRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const getPlans = require('../handlers/Plans/getPlans');
-
-
-const plansRouter = Router();
-
-plansRouter.get('/', getPlans);
-
-module.exports = plansRouter;
