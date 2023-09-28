b71634e126d462a8b9d9756282fb75dfc4284a6f
N-Cano <nahuelcano2009@gmail.com>
2023-09-28 11:14:23 -0300

    Comienzo de redux

--- client/src/redux/reducer.jsx
+++ client/src/redux/reducer.jsx
@@ -1,8 +1,27 @@
-const initialState = {prueba: []}
+import {
+    GET_DOCTORS,
+    GET_DOCTOR,
+    GET_PATIENT,
+} from "./action-types";
+
+const initialState = {
+    doctors: [],
+    doctor: {},
+    patient: {},
+}
 const rootReducer = (state = initialState, actions) => {
     switch (actions.type) {
+        case GET_DOCTORS:
+            return { ...state, doctors: actions.payload }
+
+        case GET_DOCTOR:
+            return { ...state, doctor: actions.payload }
+
+        case GET_PATIENT:
+            return { ...state, patient: actions.payload }
+
         default:
-            return {...state}
+            return { ...state }
     }
 }
 export default rootReducer
