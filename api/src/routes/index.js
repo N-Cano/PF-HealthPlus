1fb9a0601042c9528d2b22e8679d5480693216b8
Manu <manuharris0@gmail.com>
2023-09-28 11:10:36 -0300

    feat(api/src): get user by id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 
 const doctorsRouter = require('./doctorsRouter');
-const plansRouter = require('./plansRouter');
+// const plansRouter = require('./plansRouter');
 const usersRouter = require('./usersRouter');
 const datesRouter = require('./datesRouter');
 
 const routes = Router();
 
 routes.use('/doctors', doctorsRouter);
-routes.use('/plans', plansRouter);
+// routes.use('/plans', plansRouter);
 routes.use('/users', usersRouter);
 routes.use('/dates', datesRouter);
 
