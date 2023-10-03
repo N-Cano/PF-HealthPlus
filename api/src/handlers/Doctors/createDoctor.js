3a7a846d4bfbb80ba9168cf7b44a021b5169dbac
Nacho Polinari <nachopolinari@gmail.com>
2023-10-03 10:07:37 -0300

    fix (api/createDoctors): validations createDoctors

--- api/src/handlers/Doctors/createDoctor.js
+++ api/src/handlers/Doctors/createDoctor.js
@@ -4,8 +4,19 @@ const { createDoctor } = require("../../controllers/doctorsControllers");
 const postDoctor = async (req, res) => {
     try {
         const { description, enable, image, name, price, specialty } = req.body;
+
+        //validacion de tipos de datos BD
+        if (
+            typeof description !== 'string' ||
+            typeof image !== 'string' ||
+            typeof name !== 'string' ||
+            typeof specialty !== 'string' ||
+            typeof enable !== 'boolean' ||
+            (typeof price !== 'number' || isNaN(price))
+        ) throw new Error('Data types are not valid to create a new doctor');
+
         //TODO condicionales
-        const data = {description, enable, image, name, price, specialty};
+        const data = { description, enable, image, name, price, specialty };
         const newDoctor = await createDoctor(data);
         res.status(201).json(newDoctor)
     } catch (error) {
