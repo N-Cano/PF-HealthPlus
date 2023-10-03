7a129e811b45729ba82a981b944ac6845c02e10a
PSGamezG <paulita.gorillaz@gmail.com>
2023-10-03 15:31:49 -0500

    feat (client/home) changed de subscribe section

--- client/src/Views/Payment/Payment.jsx
+++ client/src/Views/Payment/Payment.jsx
@@ -1,8 +1,8 @@
 const Payment = () => {
-    return (
-        <div>
-            <h1>Payment</h1>
-        </div>
-    )
-}
-export default Payment 
+  return (
+    <div>
+      <h1>Payment</h1>
+    </div>
+  );
+};
+export default Payment;
