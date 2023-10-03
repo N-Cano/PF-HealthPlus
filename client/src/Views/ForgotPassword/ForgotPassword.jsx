3b035eae3cfb79d36872aca52211da7977078102
N-Cano <nahuelcano2009@gmail.com>
2023-10-03 16:28:49 -0300

    fix (client/src/views) validaciones de todos los forms terminado

--- client/src/Views/ForgotPassword/ForgotPassword.jsx
+++ client/src/Views/ForgotPassword/ForgotPassword.jsx
@@ -1,50 +1,65 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { Link } from "react-router-dom"
-import { sendPasswordResetEmail} from "firebase/auth";
+import { Link } from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../../firebase/firebase.config";
 import { useNavigate } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
 
-const forgotPassword = () => {
-    const loginPage = useNavigate();
-    const handleSubmit = async(e)=>{
+const ForgotPassword = () => {
+    const navigate = useNavigate();
+    const { handleSubmit, control, formState: { errors } } = useForm();
 
-        e.preventDefault()
-
+    const onSubmit = async (data) => {
         try {
-          const emailValue = e.target.email.value;
-          await sendPasswordResetEmail(auth, emailValue);
-          alert("Sending email to restart your password!!")
-          loginPage("/login");
+            await sendPasswordResetEmail(auth, data.email);
+            navigate("/login");
         } catch (error) {
-        alert(error.message);
-        }          
+            console.log(error.message);
+        }
     };
-    
+
     return (
-        <form onSubmit={handleSubmit} className='flex m-auto justify-center items-center flex-col h-screen'>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex m-auto justify-center items-center flex-col h-screen bg-blue-200'>
             <Link
                 to='/login'
                 className='absolute top-0 left-0 m-8 hover:scale-110 transition duration-300 ease-in-out bg-slate-950 text-neutral-50 w-20 p-2 rounded-2xl text-center'
-            >Back</Link>
+            >
+                Back
+            </Link>
 
             <div className='flex justify-center flex-col gap-2 mb-6 max-w-md w-full h-5/6'>
                 <div className='flex flex-col items-center gap-8 bg-blue-400 p-8 rounded-2xl'>
                     <label className='font-bold text-2xl'>Email</label>
-                    <input
-                        className='p-2 pl-4 placeholder-slate-600 rounded-2xl focus:outline-none w-72'
-                        type="text"
+                    <Controller
                         name="email"
-                        placeholder="Email..."
-                    ></input>
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                            required: "Este campo es obligatorio",
+                            pattern: {
+                                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                                message: "Ingrese una dirección de correo electrónico válida",
+                            },
+                        }}
+                        render={({ field }) => (
+                            <input
+                                {...field}
+                                className='p-2 pl-4 placeholder-slate-600 rounded-2xl focus:outline-none w-72'
+                                type="email"
+                                placeholder="Email..."
+                            />
+                        )}
+                    />
+                    {errors.email && <p className='text-red-800'>{errors.email.message}</p>}
                     <button
                         type="submit"
                         className='bg-slate-950 text-neutral-50 w-24 p-2 rounded-2xl hover:bg-slate-700 hover:scale-110 transition duration-300 ease-in-out'
-                    >Send Email</button>
+                    >
+                        Send Email
+                    </button>
                 </div>
             </div>
-
         </form>
-    )
-}
+    );
+};
 
-export default forgotPassword
+export default ForgotPassword;
