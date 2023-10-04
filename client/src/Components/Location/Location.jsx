af91d784381627d4abbd85668a4f3944c8dab5df
Katerin <katerinmonguacastro@gmail.com>
2023-10-04 09:10:00 -0500

    feat(client/sponsors) add new layout to sponsors and landing

--- client/src/Components/Location/Location.jsx
+++ client/src/Components/Location/Location.jsx
@@ -1,14 +1,13 @@
+import { FaMapLocationDot } from "react-icons/fa6";
 import { HiOutlinePhoneOutgoing } from "react-icons/hi";
-import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineMail } from "react-icons/ai";
-// import Map from "./Map/Map";
 import MapLeaflet from "./MapLeaflet/MapLeaflet";
 
 const Location = () => {
   return (
-    <div className="p-4 shadow-md w-full h-[485px] grid grid-cols-2 gap-3 ">
-      <div className="bg-white  h-[300px] w-[320px] flex flex-col gap-3 items-center justify-center rounded-3xl ml-[20px] mt-[68px]">
-        <CiLocationOn size={21} />
+    <div className="p-4 shadow-md grid grid-cols-1 md:grid-cols-2 gap-3">
+      <div className="bg-white ml-[150px] md:mr-[120px] md:h-[300px] w-[700px] md:w-[320px] flex flex-col gap-3 items-center justify-center rounded-3xl mt-[50px] md:mb-0">
+        <FaMapLocationDot size={21} />
         <p className="text-1xl">Kr 20 Saint Lou</p>
         <HiOutlinePhoneOutgoing size={21} />
         <p className="text-1xl">{`+(1) 7-25`}</p>
@@ -16,8 +15,7 @@ const Location = () => {
         <p className="text-1xl">healthplushclinic@gmail.com</p>
       </div>
 
-      <div className="mr-[95px]">
-        {/* <Map /> */}
+      <div className="w-full md:mr-[95px] ">
         <MapLeaflet />
       </div>
     </div>
