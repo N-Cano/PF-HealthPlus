74b0ebbf2e73f7e69b6b3efcee43491f9f1e2013
LeonardoMicale <leomicale.lm@gmail.com>
2023-10-05 14:49:28 -0300

    feat(api/controller_router_client/views/plan)

--- api/src/controllers/plansControllers.js
+++ api/src/controllers/plansControllers.js
@@ -16,3 +16,37 @@
 //     }
 // };
 // module.exports = {bringPlans};
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+    access_token: 'TEST-8033250124943304-100419-9f5940470a6ee188e569925fef444342-1500823282'
+})
+
+const plansController = (req, res) => {
+    const product = req.body;
+
+    const preference = {
+        items: [{
+            id: 1,
+            title: product.title,
+            currency_id: 'ARS',
+            picture_url: product.image,
+            description: product.description,
+            category_id: 'art',
+            quantity: 1,
+            unit_price: product.price
+    }],
+    back_urls: {
+        success: 'http://localhost:5173',
+        failure: '',
+        pending: '',
+    },
+    auto_return: 'approved',
+    binary_mode: true,
+    }
+    mercadopago.preferences.create(preference)
+    .then((response) => res.status(200).send({response}))
+    .catch((error) => res.status(400).send({error: error.message}))
+}
+
+module.exports = plansController;
--- client/src/Views/Plan/Plan.jsx
+++ client/src/Views/Plan/Plan.jsx
@@ -1,8 +1,56 @@
+import axios from 'axios';
+
 const Plan = () => {
-    return (
-        <div>
-            <h1>Plan</h1>
+    const products = [
+        {
+          image: "https://blogs.iadb.org/salud/wp-content/uploads/sites/15/2020/08/SPH_Newsletters_Blogs_AUG10_GS-POST.png",
+          title: 'Basic',
+          price: 100,
+          description: 'Basic plan'
+        },
+        {
+          image: "https://www.newmedicaleconomics.es/wp-content/uploads/2022/07/medicine-doctor-hand-working-with-modern-computer-interface-1-scaled.jpg",
+          title: 'Premium',
+          price: 200,
+          description: 'Premium plan'
+        },
+      ]
+    
+      return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+          <h1 style={{ fontSize: '3.5rem', color: 'darkviolet' }} >Medical Plans</h1>
+          {
+            products.map((product, index) => (
+              <div key={index}>
+                <h3 style={{ fontSize: '1.5rem', color: 'blue' }}>{product.title}</h3>
+                <p style={{ fontSize: '1rem' }}>{product.description}</p>
+                <p style={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'green' }}>{product.price} ARS</p>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  style={{ width: '500px', height: '300px', border: '1px solid #ccc' }}
+                />
+                <button  
+                  onClick={() => {
+                    axios.post('http://localhost:3001/payment', product)
+                    .then((res) => window.location.href = res.data.response.body.init_point)
+                  }}
+                  style={{
+                  border: '1px solid #ccc',
+                  padding: '10px',
+                  backgroundColor: 'green',
+                  color: 'white',
+                  fontSize: '1rem',
+                  marginTop: '10px',
+                  cursor: 'pointer',
+                  width: '500px',
+                }}>
+                  Buy
+                </button>
+              </div>
+            ))
+          }
         </div>
-    )
-}
+      );
+    };
 export default Plan 
