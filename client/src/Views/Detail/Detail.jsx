250b628575b9232d3d703f226182207c6593f625
Katerin <katerinmonguacastro@gmail.com>
2023-10-10 12:33:44 -0400

    feat(client/footer) add footer to most views

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDoctor } from "../../redux/actions";
-import styles from './Detail.module.css';
+import styles from "./Detail.module.css";
+import Footer from "../../Components/Footer/Footer";
 const Detail = () => {
   const [detail, setDetail] = useState({});
 
@@ -17,32 +18,36 @@ const Detail = () => {
   console.log(detail);
 
   return (
-    <div>
-    <div className={styles.nuevo}>
-
-      <div className={styles.container}>
-        <h1>{`${detail.name}`}</h1>
-        <img src={detail.photo? detail.photo.secure_url : "https://fakeimg.pl/208x208/fa0848/909090?text=ERROR"}  />
-        <div>
+    <>
+      <div className={styles.nuevo}>
+        <div className={styles.container}>
+          <h1>{`${detail.name}`}</h1>
+          <img
+            src={
+              detail.photo
+                ? detail.photo.secure_url
+                : "https://fakeimg.pl/208x208/fa0848/909090?text=ERROR"
+            }
+          />
+          <div>
             <h2>specialty:{detail.specialty}</h2>
-        </div>
-        <div>
+          </div>
+          <div>
             <h2>price: {detail.price}</h2>
-        </div>
-        <div>
+          </div>
+          <div>
             <h2>description:{detail.description}</h2>
+          </div>
+        </div>
+
+        <div className={styles.inputbox}>
+          <Link to="/home">
+            <button>Home</button>
+          </Link>
         </div>
       </div>
-      
-      <div className={styles.inputbox} >
-        <Link to="/home">
-          <button >Home</button>
-        </Link>
-      </div>
-
-    </div>
-    
-    </div>
+      <Footer />
+    </>
   );
 };
 export default Detail;
