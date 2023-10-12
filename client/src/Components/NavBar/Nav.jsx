0eee324f64c2fdedd49d223b1b99c5bd47188e76
YeesIam <lmcmail03@gmail.com>
2023-10-11 22:15:42 -0300

    Navbar desplegable

--- client/src/Components/NavBar/Nav.jsx
+++ client/src/Components/NavBar/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <Link to='/home' className="flex flex-shrink-0 items-center">
+            <Link to="/home" className="flex flex-shrink-0 items-center">
               <img className="h-10 w-auto" src={logo2} alt="Logo" />
             </Link>
 
@@ -21,7 +21,6 @@ const Nav = () => {
                 </a>
               </Link>
             </div>
-
           </div>
           <div className="text-center">
             {" "}
