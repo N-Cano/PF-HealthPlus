18a5ff12968bb42802929c92ef95df649dadb1c9
Nacho Polinari <nachopolinari@gmail.com>
2023-10-13 18:40:04 -0300

    fix(client/LanguageSwitcher) Language LocalStorage

--- client/src/Components/LanguageSwitcher/LanguageSwitcher.jsx
+++ client/src/Components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 
 const languages = {
   en: { nativeName: "English", surname: "ENG" },
@@ -7,21 +8,28 @@ const languages = {
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
-  // Se puede implementar localstorage
+
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem("lng");
+    if (storedLanguage) {
+      i18n.changeLanguage(storedLanguage);
+    }
+  }, [i18n]);
+
   const handleChangeLanguage = (lng) => {
     i18n.changeLanguage(lng);
-    // localStorage.setItem("lng", lng);
+    localStorage.setItem("lng", lng);
   };
 
   return (
     <div
       className="text-white flex gap-0.5"
       style={{ fontFamily: "Rubik, sans-serif" }}>
-      {Object.keys(languages).map((lng) => (
+      {Object.keys(languages).map((lng, index) => (
         <button
           className="hover:bg-gray-700 cursor-pointer hover:text-white rounded-md px-4 py-2 text-sm font-medium"
           type="submit"
-          key={lng}
+          key={index}
           onClick={() => handleChangeLanguage(lng)}
           disabled={i18n.resolvedLanguage === lng}>
           {languages[lng].surname}
@@ -29,6 +37,6 @@ const LanguageSwitcher = () => {
       ))}
     </div>
   );
-};
 
+}
 export default LanguageSwitcher;
