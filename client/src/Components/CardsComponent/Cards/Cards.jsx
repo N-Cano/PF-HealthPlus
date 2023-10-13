0451478794f0eee45be87a159e232d5219fb937a
N-Cano <nahuelcano2009@gmail.com>
2023-10-13 12:35:16 -0300

    fix delay cardsDoctor

--- client/src/Components/CardsComponent/Cards/Cards.jsx
+++ client/src/Components/CardsComponent/Cards/Cards.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDoctors } from "../../../redux/actions";
 import Card from "../Card/Card";
-import { useState } from "react";
 import Paginado from "../../Paginado/paginado";
+
 const Cards = () => {
   const dispatch = useDispatch();
   const doctors = useSelector((state) => state.doctors);
@@ -12,29 +12,27 @@ const Cards = () => {
     dispatch(getDoctors());
   }, [dispatch]);
 
-  const [currentPage, setCurrentPage] = useState(1); //currentpage almacena la pagina actual y el Set la actualiza
-
-  const itemsPerPage = 5; //cant de elem por pag
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
   const totalItems = doctors.length;
-  const totalPages = Math.ceil(totalItems / itemsPerPage); //calcula el total de elementos
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber); //esta func se encarga de pasar de pag
+    setCurrentPage(pageNumber);
   };
-  const startIndex = (currentPage - 1) * itemsPerPage; //calcula los indices de inicio
-  const endIndex = startIndex + itemsPerPage; //y fin
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
   const currentDoctors = doctors.slice(startIndex, endIndex);
 
   return (
     <div className="flex flex-col max-w-screen-xl mx-auto mb-0">
-      {/* {console.log("total doctors", doctors)} */}
-      <div className=" flex justify-center bg-blue-900 item rounded-2xl gap-6 h-70 mb-5 flex-wrap">
+      <div className="flex justify-center bg-blue-900 item rounded-2xl gap-6 h-70 mb-5 flex-wrap">
         {currentDoctors.map((doc) => (
           <Card
+            key={doc.id}
             name={doc.name}
             specialty={doc.specialty}
             price={doc.price}
-            key={doc.id}
             photo={doc.photo.secure_url}
             description={doc.description}
             id={doc.id}
@@ -53,4 +51,5 @@ const Cards = () => {
     </div>
   );
 };
+
 export default Cards;
